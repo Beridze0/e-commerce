91751676c3a75f39454b64fdfc9c3c45907ef8c2
Beridze0 <beridzedavit904@gmail.com>
2025-02-19 07:20:37 -0800

    added input component

--- src/components/ui/TextField.jsx
+++ src/components/ui/TextField.jsx
@@ -1,8 +1,10 @@
 const TextField = () => {
   return (
-    <div>
-        
-    </div>
+    <input 
+        type="text" 
+        className="w-64 border text-sm placeholder:text-textMuted py-1 px-2 rounded outline-none"
+        placeholder="Search product..."
+        />
   )
 }
 
