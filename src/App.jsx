239276e634520b8ba2d94d490facd30ff785e1cf
Beridze0 <beridzedavit904@gmail.com>
2025-02-22 11:17:54 -0800

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import AdBanner from "./components/features/adBanner";
-import Featured from "./components/features/Featured";
-import Navbar from "./components/layout/Navbar";
-
 const App = () => {
   return (
-    <div className="w-full text-primary">
-      <Navbar />
-      <div className="mx-20">
-        <AdBanner />
-        <Featured />
-      </div>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
